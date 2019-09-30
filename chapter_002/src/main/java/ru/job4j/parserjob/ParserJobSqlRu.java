210bc72d0fe558a011808a68614dcd561b87d73d
ogneyar <maksimus.s@mail.ru>
2019-09-30 21:47:54 +0300

    DeleetSomeUnuseFileFromParserSql

--- chapter_002/src/main/java/ru/job4j/parserjob/ParserJobSqlRu.java
+++ chapter_002/src/main/java/ru/job4j/parserjob/ParserJobSqlRu.java
@@ -6,7 +6,6 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-import ru.job4j.parserjob.workjsoup.ConectionJsoupGetingDocument;
 
 import java.io.File;
 import java.io.IOException;
