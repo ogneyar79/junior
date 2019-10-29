54f15bdc693660709df32d91d90235c6329616eb
ogneyar <maksimus.s@mail.ru>
2019-10-29 08:30:09 +0300

    AnalysFirst

---
+++ chapter_003/src/main/java/ru/job4j/io/Analizy.java
@@ -0,0 +1,119 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+/**
+ * The class for analysis log file.
+ *
+ * @author Maksim Sirotkin(maksimus.s@mail.ru)
+ * @version 0.1$
+ * @since 0.1
+ * 28.10.2019
+ */
+public class Analizy {
+
+    /**
+     * Field boolean that show, we hava data for analys or not.
+     */
+    private Boolean dataForAnalisIs = false;
+
+    /**
+     * Function returned from map value via key.
+     *
+     * @return Boolean dataForAnalisIs.
+     */
+    public Boolean getDataForAnalisIs() {
+        return dataForAnalisIs;
+    }
+
+    /**
+     * Function returned StringBuilder from  BufferedReader object.
+     *
+     * @param read BufferedReader.
+     * @return StringBuilder result.
+     */
+    public StringBuilder createAnalizFromBuferReaderToSb(BufferedReader read) throws IOException {
+        String line;
+        StringBuilder result = new StringBuilder();
+        while ((line = read.readLine()) != null) {
+            if (line.contains("400") | line.contains("500")) {
+                dataForAnalisIs = true;
+                int temp = line.contains("400") ? line.lastIndexOf("400") : line.lastIndexOf("500");
+                result.append(" Begining Of Period ");
+                result.append(line.substring(temp));
+
+                line = read.readLine();
+                while (true) {
+                    if (!(line.contains("400") | line.contains("500"))) {
+                        result.append(" End Of Period ");
+                        result.append(line.substring(temp));
+                        result.append(System.lineSeparator());
+                        break;
+                    }
+                    line = read.readLine();
+                }
+            }
+        }
+        return result;
+    }
+
+    /**
+     * procedure for sending StringBuilder to file on hard Disk.
+     *
+     * @param model  StringBuilder.
+     * @param target String, where we want to write.
+     */
+    public void sendOutfileWriter(StringBuilder model, String target) throws IOException {
+        File file = new File(target);
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+        try (PrintWriter printWriter = new PrintWriter(new FileOutputStream(file)) {
+        }) {
+            printWriter.write(model.toString());
+        }
+    }
+
+    /**
+     * procedure for sending StringBuilder to file on hard Disk.
+     *
+     * @param source String that is the path to file on hard disk file to transfer.
+     * @param target String, where we want to write our file.
+     */
+    public void unavailable(String source, String target) throws IOException {
+
+        try (BufferedReader read = new BufferedReader(new FileReader(source))) {
+            StringBuilder ourLog = this.createAnalizFromBuferReaderToSb(read);
+            if (dataForAnalisIs) {
+                this.sendOutfileWriter(ourLog, target);
+            } else {
+                this.sendOutfileWriter(new StringBuilder("No ceaseTion working of Server"), target);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        int a = 9;
+        int b = 10;
+        if (a < 10) {
+            System.out.println("This A" + a);
+            if (b < 10) {
+                System.out.println(" A and B < 10" + a + b);
+            } else {
+                System.out.println(" !!!!!");
+            }
+        }
+        System.out.println(" NoThing");
+        try (PrintWriter out = new PrintWriter(new FileOutputStream("unavailable.csv"))) {
+            out.println("15:01:30;15:02:32");
+            out.println("15:10:30;23:12:32");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ chapter_003/src/test/java/ru/job4j/io/AnalizyTest.java
@@ -0,0 +1,73 @@
+package ru.job4j.io;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import static junit.framework.TestCase.assertEquals;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+
+public class AnalizyTest {
+
+    public Analizy analizy;
+
+    String source = Analizy.class.getResource("/serverLog.txt").getFile();
+    String target = "C:\\projects\\sirotkinmaksim\\chapter_003\\src\\main\\resources\\target.csv";
+    String empty = Analizy.class.getResource("/empty.txt").getFile();
+
+    StringBuilder checkingSBuilder;
+
+    public StringBuilder createStingBulderForChecking() {
+        StringBuilder rezult = new StringBuilder(50);
+        rezult.append(" Begining Of Period 400 10:58:01 End Of Period 200 10:59:01");
+        rezult.append(System.lineSeparator());
+        rezult.append(" Begining Of Period 500 11:01:02 End Of Period 200 11:02:02");
+        rezult.append(System.lineSeparator());
+        rezult.append(" Begining Of Period 500 11:02:04 End Of Period 200 11:02:08");
+        rezult.append(System.lineSeparator());
+        System.out.println(rezult.toString());
+        return rezult;
+    }
+
+    @Before
+    public void init() {
+        analizy = new Analizy();
+        checkingSBuilder = createStingBulderForChecking();
+    }
+
+    @Test
+    public void createStringBuilderFromBuferReader() {
+        String line;
+        try (BufferedReader read = new BufferedReader(new FileReader(source))) {
+            StringBuilder builder = analizy.createAnalizFromBuferReaderToSb(read);
+            assertEquals(checkingSBuilder.toString(), builder.toString());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void checkRiteOut() throws IOException {
+        StringBuilder model = checkingSBuilder;
+        System.out.println(model.toString());
+        analizy.sendOutfileWriter(model, target);
+    }
+
+    @Test
+    public void unvailableConsistTwoConecteMethodChectedTheirConection() throws IOException {
+
+        analizy.unavailable(empty, target);
+
+        assertThat(false, is (analizy.getDataForAnalisIs()));
+
+    }
+
+}
