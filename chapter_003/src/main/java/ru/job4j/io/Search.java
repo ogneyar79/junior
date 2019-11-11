a398d2d845997f1d026067ff0c62b115588f7e61
ogneyar <maksimus.s@mail.ru>
2019-11-11 14:24:30 +0300

    ChangeMethodLookingAllFilesFromCatalog

--- chapter_003/src/main/java/ru/job4j/io/Search.java
+++ chapter_003/src/main/java/ru/job4j/io/Search.java
@@ -9,6 +9,49 @@ import java.util.Queue;
 public class Search {
 
     /**
+     * Function  returning List files from catalog without folders
+     *
+     * @param parent String The way to catalog
+     * @return result is List files is found as result.
+     */
+    private List<File> filesAll(String parent) {
+        List<File> result = new ArrayList<>();
+        Queue<File> queue = new LinkedList<>();
+        queue.offer(new File(parent));
+        while (!queue.isEmpty()) {
+            File file = queue.poll();
+            if (file.isDirectory()) {
+                System.out.println(file.listFiles().length);
+                System.out.println(file.getName() + "  \t folder");
+                for (File deep : file.listFiles()) {
+                    queue.offer(deep);
+                }
+            } else if (file.isFile()) {
+                result.add(file);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Function of looking for a list of files with expansion inner  method we use private method filesAll() returning files from catalog without folder
+     *
+     * @param parent    String The way to catalog
+     * @param expansion List expansion our files that we looking for.
+     * @return result is List files is found as result.
+     */
+    public List<File> filesNewVersion(String parent, List<String> expansion) {
+        List<File> listFiles = filesAll(parent);
+        List<File> result = new ArrayList<>();
+        for (File file : listFiles) {
+            if (checkIdentityExts(file, expansion)) {
+                result.add(file);
+            }
+        }
+        return result;
+    }
+
+    /**
      * Function of looking for a list of files with expansion
      *
      * @param parent    String The way to catalog
@@ -22,14 +65,14 @@ public class Search {
         while (!queue.isEmpty()) {
             File file = queue.poll();
             if (file.isDirectory()) {
-                System.out.println(file.getName() + "  \t folder");
+                System.out.println(file.listFiles().length);
+                //    System.out.println(file.getName() + "  \t folder");
                 for (File deep : file.listFiles()) {
                     queue.offer(deep);
                 }
-            }
-            if (file.isFile() && this.checkIdentityExts(file, expansion)) {
+            } else if (this.checkIdentityExts(file, expansion)) {
                 result.add(file);
-                System.out.println(file.getName() + "\t file");
+                //       System.out.println(file.getName() + "\t file");
             }
         }
         return result;
@@ -43,9 +86,11 @@ public class Search {
     private boolean checkIdentityExts(File file, List<String> expansion) {
 
         boolean result = false;
-        for (String eXp : expansion) {
-            if (file.getName().endsWith(eXp)) {
-                result = true;
+        if (file.isFile()) {
+            for (String eXp : expansion) {
+                if (file.getName().endsWith(eXp)) {
+                    result = true;
+                }
             }
         }
         return result;
