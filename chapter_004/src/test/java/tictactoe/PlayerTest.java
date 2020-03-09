44299b06d7894e83ea3a47d902ea902461d4770a
ogneyar <maksimus.s@mail.ru>
2020-03-09 19:57:49 +0300

    TicAddTestPlayer

---
+++ chapter_004/src/test/java/tictactoe/PlayerTest.java
@@ -0,0 +1,32 @@
+package tictactoe;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PlayerTest {
+
+    @Test
+    public void getName() throws Exception {
+        final String nameInput = " Rob";
+        final String expected = nameInput;
+
+        Player player = new Player(nameInput, null);
+        final String actualName = player.getName();
+        assertEquals(expected, actualName);
+    }
+
+    @Test
+    public void getFigure() throws Exception {
+        final  Figure inputValue = Figure.X;
+        final  Figure expected = inputValue;
+
+        final Player player = new Player(null, inputValue);
+
+        final Figure actualFigure = player.getFigure();
+
+        assertEquals(expected, actualFigure);
+
+    }
+
+}
