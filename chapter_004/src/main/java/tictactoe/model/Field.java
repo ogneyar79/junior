ce9d6b7de0b56796a190e4e9dc38684e0be1f50f
ogneyar <maksimus.s@mail.ru>
2020-03-17 20:42:43 +0300

    DeleteStaticFieldChangeCheckPointCAnMakeDifferenteLengtch

--- chapter_004/src/main/java/tictactoe/model/Field.java
+++ chapter_004/src/main/java/tictactoe/model/Field.java
@@ -7,11 +7,17 @@ import java.awt.*;
 
 public class Field {
 
-    private static final int FIELD_SIZE = 3;
+    private final int FIELD_SIZE;
     private static final int MIN_COORDINATE = 0;
-    private static final int MAX_COORDINATE = FIELD_SIZE;
 
-    private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
+
+    private final Figure[][] field;
+
+    public Field(final int field_size) {
+        FIELD_SIZE = field_size;
+        this.field = new Figure[FIELD_SIZE][FIELD_SIZE];
+    }
+
 
     public int getFieldSize() {
         return FIELD_SIZE;
@@ -33,10 +39,14 @@ public class Field {
     }
 
     private boolean checkPoint(Point point) {
-        return checkCoordinate(point.x) && checkCoordinate(point.y);
+        return checkCoordinate(point.x, field.length) && checkCoordinate(point.y, field[point.x].length);
     }
 
-    private boolean checkCoordinate(final int coordinate) {
+    private boolean checkCoordinate(final int coordinate, final int MAX_COORDINATE ) {
         return coordinate >= MIN_COORDINATE && coordinate <= MAX_COORDINATE;
+    }
+
+    public Figure[][] getField() {
+        return field;
     }
 }
