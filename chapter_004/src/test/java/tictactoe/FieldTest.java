eada88ed7ede1d8cee41da8914b26bdd1bd8394b
ogneyar <maksimus.s@mail.ru>
2020-03-17 20:44:43 +0300

    TestGetWinnerForFourMethod

--- chapter_004/src/test/java/tictactoe/FieldTest.java
+++ chapter_004/src/test/java/tictactoe/FieldTest.java
@@ -2,7 +2,6 @@ package tictactoe;
 
 import org.junit.Test;
 import tictactoe.model.exeption.InvalidPointException;
-import tictactoe.model.exeption.OccupiedException;
 import tictactoe.model.Field;
 import tictactoe.model.Figure;
 
@@ -13,13 +12,15 @@ import static org.junit.Assert.*;
 public class FieldTest {
     @Test
     public void getFieldSize() throws Exception {
-        final Field field = new Field();
+        int field_size = 3;
+        final Field field = new Field(field_size);
         assertEquals(3, field.getFieldSize());
     }
 
     @Test
     public void getFigureWhenFigureNoSet() throws InvalidPointException {
-        final Field field = new Field();
+        int field_size = 3;
+        final Field field = new Field(field_size);
         final Point inputPoint = new Point(0, 0);
         final Figure actualFigure = field.getFigure(inputPoint);
         assertEquals(null, actualFigure);
@@ -27,7 +28,8 @@ public class FieldTest {
 
     @Test
     public void whenPointIncorecct() {
-        final Field field = new Field();
+        int field_size = 3;
+        final Field field = new Field(field_size);
         final Point inputPoint = new Point(-1, 0);
         try {
             field.getFigure(inputPoint);
@@ -39,7 +41,8 @@ public class FieldTest {
 
     @Test
     public void whenPointYmoreThenSize() {
-        final Field field = new Field();
+        int field_size = 3;
+        final Field field = new Field(field_size);
         final Point inputPoint = new Point(0, field.getFieldSize() + 2);
         try {
             field.getFigure(inputPoint);
@@ -51,13 +54,12 @@ public class FieldTest {
 
     @Test
     public void setFigure() throws Exception {
-        final Field field = new Field();
+        int field_size = 3;
+        final Field field = new Field(field_size);
         final Point inputPoint = new Point(0, 0);
         final Figure inputFigure = Figure.O;
         field.setFigure(inputPoint, inputFigure);
         final Figure actualFigure = field.getFigure(inputPoint);
         assertEquals(inputFigure, actualFigure);
     }
-
-
 }
