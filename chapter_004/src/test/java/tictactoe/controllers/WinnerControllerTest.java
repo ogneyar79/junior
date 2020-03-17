8a4379ddac5610de2b2348f61cc9e97640598b51
ogneyar <maksimus.s@mail.ru>
2020-03-17 08:28:12 +0300

    TestGetWinnerForFourMethod

---
+++ chapter_004/src/test/java/tictactoe/controllers/WinnerControllerTest.java
@@ -0,0 +1,80 @@
+package tictactoe.controllers;
+
+import org.junit.Test;
+import tictactoe.model.Field;
+import tictactoe.model.Figure;
+import tictactoe.model.exeption.InvalidPointException;
+
+import java.awt.*;
+
+
+import static org.junit.Assert.*;
+
+public class WinnerControllerTest {
+
+    @Test
+    public void changeFigure() throws Exception {
+        WinnerController winnerControl = new WinnerController();
+        Figure figureX = Figure.X;
+        winnerControl.changeFigure(figureX);
+        Figure resultO = winnerControl.changeFigure(figureX);
+        assertEquals(resultO.equals(Figure.O), (true));
+    }
+
+
+    @Test
+    public void checkDiagonal() throws Exception {
+        Field field = new Field();
+        Figure figure = Figure.X;
+        Figure figureO = Figure.O;
+        field.setFigure(new Point(0, 0), figure);
+
+
+        field.setFigure(new Point(1, 1), figure);
+        field.setFigure(new Point(2, 2), figure);
+        field.setFigure(new Point(0, 1), figureO);
+        field.setFigure(new Point(0, 2), figureO);
+
+//        for (int i = 0; i < field.getField().length; i++) {
+//            for (int j = 0; j < field.getField()[i].length; j++) {
+//                System.out.println(field.getField()[i][j].toString());
+//            }
+//        }
+        WinnerController winnerControl = new WinnerController();
+        Boolean fact = winnerControl.checkDiagonal(field, figure);
+        boolean expect = true;
+        assertEquals(expect, (fact));
+    }
+
+    @Test
+    public void checkLanes() throws Exception {
+        Field field = new Field();
+        Figure figure = Figure.X;
+        Figure figureO = Figure.O;
+        field.setFigure(new Point(0, 0), figure);
+        field.setFigure(new Point(1, 1), figure);
+        field.setFigure(new Point(2, 2), figure);
+        field.setFigure(new Point(0, 1), figureO);
+        field.setFigure(new Point(0, 2), figureO);
+        WinnerController winnerControl = new WinnerController();
+        Boolean fact = winnerControl.checkLanes(field, figure);
+        boolean expect = true;
+        assertEquals(expect, (fact));
+    }
+    @Test
+    public void checkWinnerFigure() throws InvalidPointException {
+        Field field = new Field();
+        Figure figure = Figure.X;
+        Figure figureO = Figure.O;
+        field.setFigure(new Point(0, 0), figure);
+        field.setFigure(new Point(1, 1), figure);
+        field.setFigure(new Point(2, 2), figure);
+        field.setFigure(new Point(0, 1), figureO);
+        field.setFigure(new Point(0, 2), figureO);
+        WinnerController winnerControl = new WinnerController();
+        Figure factGetiingFigure = winnerControl.getWinner(field).get();
+        System.out.println(factGetiingFigure.toString());
+        assertEquals(figure, (factGetiingFigure));
+    }
+
+}
