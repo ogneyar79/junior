122ab649ed384374917c4101b49d4f9ae11cfa11
ogneyar <maksimus.s@mail.ru>
2020-05-08 15:01:08 +0300

    IInputCoordinate

--- chapter_004/src/main/java/tictactoe/view/reader/IInputCoordinate.java
+++ chapter_004/src/main/java/tictactoe/view/reader/IInputCoordinate.java
@@ -1,11 +1,12 @@
 package tictactoe.view.reader;
 
 import tictactoe.model.exeption.InvalidFigureWriting;
+import tictactoe.model.exeption.InvalidPointException;
 
 import java.awt.*;
 
 public interface IInputCoordinate {
-    public Point inputPoint() throws InvalidFigureWriting;
+    public Point inputPoint() throws InvalidFigureWriting, InvalidPointException;
 
 
 }
