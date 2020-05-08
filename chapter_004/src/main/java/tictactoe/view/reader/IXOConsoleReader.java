8c1d3f48de07357b39ee654401effd0dc8a42b0b
ogneyar <maksimus.s@mail.ru>
2020-05-08 15:02:23 +0300

    IXOConsoleReader

--- chapter_004/src/main/java/tictactoe/view/reader/IXOConsoleReader.java
+++ chapter_004/src/main/java/tictactoe/view/reader/IXOConsoleReader.java
@@ -1,8 +1,9 @@
 package tictactoe.view.reader;
 
 import tictactoe.model.exeption.InvalidFigureWriting;
+import tictactoe.model.exeption.InvalidPointException;
 
 public interface IXOConsoleReader {
 
-    int askCoordinate(final String coordinateName) throws InvalidFigureWriting;
+    int askCoordinate(final String coordinateName) throws InvalidFigureWriting, InvalidPointException;
 }
