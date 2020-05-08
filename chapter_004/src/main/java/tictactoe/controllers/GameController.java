e45a38aca863d76eb33253f9b922bd8974b2b629
ogneyar <maksimus.s@mail.ru>
2020-05-08 15:06:02 +0300

    GameControllerQ

--- chapter_004/src/main/java/tictactoe/controllers/GameController.java
+++ chapter_004/src/main/java/tictactoe/controllers/GameController.java
@@ -3,12 +3,11 @@ package tictactoe.controllers;
 import tictactoe.model.Field;
 import tictactoe.model.Figure;
 import tictactoe.model.Game;
-import tictactoe.model.Player;
 import tictactoe.model.exeption.InvalidFigure;
 import tictactoe.model.exeption.InvalidFigureWriting;
+import tictactoe.model.exeption.InvalidPointException;
+import tictactoe.model.exeption.OccupiedException;
 import tictactoe.view.ConsoleView;
-import tictactoe.view.reader.IXOConsoleReader;
-import tictactoe.view.reader.XOReader;
 
 import java.awt.*;
 import java.util.Optional;
@@ -16,7 +15,7 @@ import java.util.Optional;
 /**
  * Class that aggregate our Controllers that we can play.
  */
-public class GameController {
+public class GameController implements IGameController{
 
     /**
      * object that define show our game.
@@ -41,35 +40,43 @@ public class GameController {
      * @throws InvalidFigure
      */
 
-    public void aggregatePlay(Game game) throws InvalidFigure, InvalidFigureWriting {
+    public void aggregatePlay(Game game) throws InvalidFigure, InvalidFigureWriting, InvalidPointException, OccupiedException {
         this.consoleView.show(game);
         Field field = game.getField();
         while (moveController.controlMove(field)) {
             final Figure currentF = moveController.getCurrentMoveController().getCurrentFigure().get();
             System.err.format("Please  enter move Point for: %s\n", currentF);
             Point point = this.consoleView.askPoint();
+            while (!field.checkPoint(point) & !(field.checkFreeOccupied(point))) {
+                System.err.println("Put walid coordinate");
+                point = this.consoleView.askPoint();
+            }
+
             moveController.moveFigure(field, point, currentF);
+
+
             this.consoleView.show(game);
         }
-        moveController.getCurrentMoveController();
+       // moveController.getCurrentMoveController();
         Optional<Figure> figure = moveController.getWinnerController().getWinner(game.getField());
         consoleView.showWinner(figure);
     }
 
-    public static void main(String... args) throws InvalidFigure, InvalidFigureWriting {
-        Player[] players = new Player[2];
-        Field field = new Field(3);
-        players[0] = new Player("ONE", Figure.X);
-        players[1] = new Player("TWO", Figure.O);
-        Game game = new Game(players, field, "FIRSTGame");
-        IXOConsoleReader reader = new XOReader();
-        ConsoleView consoleView = new ConsoleView(reader, imputer);
-        CurrentMoveController currentController = new CurrentMoveController(game.getField(), game.getFirstFigure());
-        WinnerController winnerController = new WinnerController();
-        MoveController moveController = new MoveController(currentController, winnerController);
 
-        GameController gameController = new GameController(consoleView, moveController);
-        gameController.aggregatePlay(game);
-    }
+//    public static void main(String... args) throws InvalidFigure, InvalidFigureWriting {
+//        Player[] players = new Player[2];
+//        Field field = new Field(3);
+//        players[0] = new Player("ONE", Figure.X);
+//        players[1] = new Player("TWO", Figure.O);
+//        Game game = new Game(players, field, "FIRSTGame");
+//        IXOConsoleReader reader = new XOReader();
+//        ConsoleView consoleView = new ConsoleView(reader, imputer);
+//        CurrentMoveController currentController = new CurrentMoveController(game.getField(), game.getFirstFigure());
+//        WinnerController winnerController = new WinnerController();
+//        MoveController moveController = new MoveController(currentController, winnerController);
+//
+//        GameController gameController = new GameController(consoleView, moveController);
+//        gameController.aggregatePlay(game);
+//    }
 
 }
