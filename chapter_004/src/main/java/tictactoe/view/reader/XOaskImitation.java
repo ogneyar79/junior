82d52c3becd2e853b1e54496700bf70f718b4722
ogneyar <maksimus.s@mail.ru>
2020-05-08 15:08:35 +0300

    XOaskImitation

--- chapter_004/src/main/java/tictactoe/view/reader/XOaskImitation.java
+++ chapter_004/src/main/java/tictactoe/view/reader/XOaskImitation.java
@@ -2,6 +2,12 @@ package tictactoe.view.reader;
 
 import tictactoe.model.Field;
 import tictactoe.model.exeption.InvalidFigureWriting;
+import tictactoe.model.exeption.InvalidPointException;
+
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class XOaskImitation implements IXOConsoleReader {
 
@@ -20,20 +26,45 @@ public class XOaskImitation implements IXOConsoleReader {
 
     private int arrayIndex;
 
-    @Override
-    public int askCoordinate(String coordinateName) throws InvalidFigureWriting {
-        System.out.format("Please input %s:", coordinateName);
-        int arrayIndex = (int) (Math.random() * (field.getElementCounter()));
-        this.arrayIndex = arrayIndex;
-        arrayX = arrayIndex / this.size;
-        arrayY = arrayIndex - (arrayX * this.size);
+    /**
+     * index deleted element at array if Point on field is occupied.
+     */
+    private List<Integer> indexRemoveArray = new ArrayList<>(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8));
 
+    /**
+     * function chek free cells on the field.
+     *
+     * @param point
+     * @return
+     * @throws InvalidPointException
+     */
+    public boolean checkFree(Point point) throws InvalidPointException {
+        boolean result = false;
+        if (this.field.getFigure(point) == null) {
+            result = true;
+        }
+        return result;
+    }
+
+    @Override
+    public int askCoordinate(String coordinateName) throws InvalidFigureWriting, InvalidPointException {
+      //  System.out.format("Please input %s:", coordinateName);
+        int firstRandom = (int) (Math.random() * (indexRemoveArray.size()));
+        int coodinateAtOne = indexRemoveArray.get(firstRandom);
+        this.indexRemoveArray.remove(firstRandom);
+        arrayX = coodinateAtOne / this.size;
+        arrayY = coodinateAtOne - (arrayX * this.size);
+        while (!checkFree(new Point(arrayX, arrayY))) {
+            arrayX = askCoordinate("X");
+            arrayY = getArrayY();
+        }
         if (coordinateName.equalsIgnoreCase("X")) {
             return arrayX;
         }
         if (coordinateName.equalsIgnoreCase("Y")) {
             return arrayY;
         }
+
         throw new InvalidFigureWriting();
 
     }
