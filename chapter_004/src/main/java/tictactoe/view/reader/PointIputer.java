a978fd38ec4009bf3cf5d7ec2517f35b3e6cec4d
ogneyar <maksimus.s@mail.ru>
2020-05-08 15:03:17 +0300

    PointIputer

--- chapter_004/src/main/java/tictactoe/view/reader/PointIputer.java
+++ chapter_004/src/main/java/tictactoe/view/reader/PointIputer.java
@@ -1,18 +1,21 @@
 package tictactoe.view.reader;
 
+import tictactoe.model.exeption.InvalidFigureWriting;
+import tictactoe.model.exeption.InvalidPointException;
+
 import java.awt.*;
 
 public class PointIputer implements IInputCoordinate {
 
 
-    private final XOReader reader;
+    private final IXOConsoleReader reader;
 
-    public PointIputer(XOReader reader) {
+    public PointIputer(IXOConsoleReader reader) {
         this.reader = reader;
     }
 
     @Override
-    public Point inputPoint() {
+    public Point inputPoint() throws InvalidFigureWriting, InvalidPointException {
         return new Point(this.reader.askCoordinate("X") - 1, reader.askCoordinate("Y") - 1);
     }
 }
