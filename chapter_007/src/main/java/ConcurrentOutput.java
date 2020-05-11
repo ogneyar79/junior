afc5f7dad5ba9f08f0cd627fed66df48b87f749b
ogneyar <maksimus.s@mail.ru>
2020-05-11 11:53:05 +0300

    ConcurentOutPuttFirst

---
+++ chapter_007/src/main/java/ConcurrentOutput.java
@@ -0,0 +1,15 @@
+public class ConcurrentOutput {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread another = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        another.start();
+  //      another.join(006);
+        System.out.println(Thread.currentThread().getName());
+
+        Thread second = new Thread(() -> System.out.println(Thread.currentThread().getName()));
+        second.start();
+    }
+
+}
