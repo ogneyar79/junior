fdae1f57375dda3aad9279ca8e76624b049eb440
ogneyar <maksimus.s@mail.ru>
2020-05-14 11:56:54 +0300

    WgetFirst

---
+++ chapter_007/src/main/java/concurrent/Wget.java
@@ -0,0 +1,29 @@
+package concurrent;
+
+public class Wget {
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+        Thread first = new Thread(
+
+
+                () -> {
+                    for (int index = 0; index < 100; index++) {
+                        try {
+                            Thread.sleep(1000);
+                            System.out.print("\rLoading : " + index + "%");
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }
+        );
+
+        first.start();
+
+
+    }
+}
