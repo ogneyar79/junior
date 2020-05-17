ab13fdb297037908418da3fda4c2a1a57425245f
ogneyar <maksimus.s@mail.ru>
2020-05-17 13:41:28 +0300

    CacheSinchronaizedAtomar

---
+++ chapter_007/src/main/java/concurrent/sharedresourcec/Cache.java
@@ -0,0 +1,13 @@
+package concurrent.sharedresourcec;
+
+public final class Cache {
+
+    private static  Cache cache;
+
+    private static synchronized Cache instOf() {
+        if (cache == null) {
+            cache = new Cache();
+        }
+        return cache;
+    }
+}
