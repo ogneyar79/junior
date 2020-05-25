8ea38abd1f1c95417c4ae7b02e21a442303d45b2
ogneyar <maksimus.s@mail.ru>
2020-05-25 07:35:45 +0300

    SingleLockListAdd

---
+++ chapter_007/src/main/java/concurrent/userstorage/SingleLockList.java
@@ -0,0 +1,41 @@
+package concurrent.userstorage;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+import ru.job4j.collection.list.DinamicArrayConteiner;
+
+import java.util.Arrays;
+import java.util.Iterator;
+
+@ThreadSafe
+public class SingleLockList<T> implements Iterable<T> {
+
+    @GuardedBy("this")
+    private DinamicArrayConteiner<T> list = new DinamicArrayConteiner();
+
+    public synchronized void add(T value) {
+
+        this.list.add(value);
+    }
+
+    public synchronized T get(int index) {
+        return this.list.get(index);
+    }
+
+    public synchronized DinamicArrayConteiner<T> copy(DinamicArrayConteiner<T> list) {
+        DinamicArrayConteiner copyList = new DinamicArrayConteiner();
+        Arrays.stream(list.getContainer()).filter(x -> x != null).forEach(copyList::add);
+        return copyList;
+    }
+
+    @Override
+    public synchronized Iterator<T> iterator() {
+        list.iterator();
+
+        return this.copy(this.list).iterator();
+    }
+
+    public DinamicArrayConteiner<T> getList() {
+        return list;
+    }
+}
