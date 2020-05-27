5ee1ba7564ff75693e98dbed88bdb46e2559446f
ogneyar <maksimus.s@mail.ru>
2020-05-27 22:39:06 +0300

    CounterBarrierStart

---
+++ chapter_007/src/test/java/concurrent/userstorage/CountBarrierUser.java
@@ -0,0 +1,30 @@
+package concurrent.userstorage;
+
+public class CountBarrierUser {
+    public static void main(String[] args) {
+        int total = 0;
+        CountBarrier counter = new CountBarrier(total);
+
+        Thread counterThread = new Thread(() -> {
+            counter.count();
+
+        });
+
+        Thread counterThread2 = new Thread(() -> {
+            counter.count();
+
+        });
+        Thread executorThread = new Thread(() -> {
+            System.out.println("Check condition");
+            System.out.println(" /total is" + counter.getTotal());
+            counter.await();
+            System.out.println("Executor Work");
+            System.out.println(" /total is" + counter.getTotal());
+
+        });
+
+        counterThread.start();
+        counterThread2.start();
+        executorThread.start();
+    }
+}
