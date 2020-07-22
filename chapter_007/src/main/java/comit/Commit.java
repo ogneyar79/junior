61c8db8eadc0e8959e3e21e6223189de84967e73
ogneyar <maksimus.s@mail.ru>
2020-07-22 17:39:00 +0300

    FIXERROR

--- chapter_007/src/main/java/comit/Commit.java
+++ chapter_007/src/main/java/comit/Commit.java
@@ -3,6 +3,6 @@ package comit;
 public class Commit {
 
     public static void main(String... args){
-        System.out.println("FiX Commit");
+        System.out.println("FIX ERROR 3 Commit");
     }
 }
