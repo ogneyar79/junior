2c8a666b216854a5a3f028c3c9586f3cdb304771
ogneyar79 <maksimus.s@mail.ru>
2020-07-24 12:45:13 +0300

    CorrectMistake

--- chapter_007/src/main/java/comit/Commit.java
+++ chapter_007/src/main/java/comit/Commit.java
@@ -5,5 +5,7 @@ public class Commit {
     public static void main(String... args) {
         System.out.println("FIX ERRoR5");
         System.err.println("Add new Change");
+
+        System.out.println(" Объединение зафиксированных изменений");
     }
 }
