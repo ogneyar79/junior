3cfa53e0ce1640912b6ee4a9829552c2b7412ecb
ogneyar79 <maksimus.s@mail.ru>
2021-03-25 13:34:41 +0300

    AddSomeSpringsBeans

---
+++ chapter_001/src/main/java/ru/job4j/di/BeanConfig.java
@@ -0,0 +1,25 @@
+package ru.job4j.di;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BeanConfig {
+
+
+    @Bean
+    public ConsoleInput consoleInput () {
+        return new ConsoleInput();
+    }
+    @Bean
+    public Store store(){
+        return new Store();
+    }
+
+    @Bean
+    public StartUI startUI() {
+        return new StartUI(store(),consoleInput());
+    }
+
+}
