4c3acebd79cde9f5c8ade9b686e981202fe970db
ogneyar79 <maksimus.s@mail.ru>
2021-03-26 11:37:19 +0300

    Add some properties to ConsoleInput

--- chapter_001/src/main/java/ru/job4j/di/ConsoleInput.java
+++ chapter_001/src/main/java/ru/job4j/di/ConsoleInput.java
@@ -1,8 +1,17 @@
 package ru.job4j.di;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ConsoleInput {
 
+    private List<String> list = new ArrayList<>();
 
+    public void addToList(String value) {
+        list.add(value);
+    }
 
-
+    public List<String> getList() {
+        return list;
+    }
 }
